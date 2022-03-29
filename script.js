125e3f2e9d6fb0311f6b6fc849040124300c75b7
Breno Ramos <70190197+Brenins@users.noreply.github.com>
2022-03-29 20:30:14 -0300

    botao capturar com SweetAlert e mais ajustes  css

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ const fetchPokemon = () =>{
     const pokemonPromises = []
 
     pokemonPromises.push(fetch(getPokemonUrl(pesquisa)).then(response => response.json()))
-
+    console.log("batata")
     Promise.all(pokemonPromises)
         .then(pokemons =>{
-
             const lisPokemons = pokemons.reduce((accumulator, pokemon)=>{
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name)
                 accumulator += `
                     <li class="card">
-                    <img class="card-img ${types[0]} " alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
+                    <img class="card-img ${types[0]} " alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
                         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                         <p class="card-subtitle">${types.join('|')}</p>
+                        <button class="btn btn-primary" type="button" id="captura" onclick="capturar('${pokemon.name}')">Capturar</button>
                     </li>
                 `
                 return accumulator
@@ -36,3 +36,12 @@ function reload(){
     location.reload();
 }
 
+function capturar(nomePokemon){
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: `Você capturou o ${nomePokemon}`,
+        showConfirmButton: false,
+        timer: 1500
+      })
+}
