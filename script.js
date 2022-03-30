74765f3ef12535ec6a57f34a1dd88b2358904f2b
Breno Ramos <70190197+Brenins@users.noreply.github.com>
2022-03-30 16:56:00 -0300

    melhorias no design do css

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
+//Função principal que permite a conexao com a api atraves do metodo FETCH-API.
 const fetchPokemon = () =>{
-    
+
     var pesquisa = document.getElementById("txtBusca").value;
-    pesquisa.toString;
-
+    pesquisa.toString;//Transforma o elemento da barrinha em string para que a api consiga ler normalmente.
     const getPokemonUrl = name => `https://pokeapi.co/api/v2/pokemon/${name}`
-
     const pokemonPromises = []
 
+    //Retorno de dados via promessa de conexão pela api através do FETCH.
     pokemonPromises.push(fetch(getPokemonUrl(pesquisa)).then(response => response.json()))
-    console.log(pokemonPromises)
+    console.log(pokemonPromises) //Log de apoio para pesquisa de dados no console.
+
     Promise.all(pokemonPromises)
         .then(pokemons =>{
             const lisPokemons = pokemons.reduce((accumulator, pokemon)=>{
-            const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+            const types = pokemon.types.map(typeInfo => typeInfo.type.name); //Array trazido da api pela funcio MAP, funcionando tipo While.
             const habilidade = pokemon.abilities.map(abilitiesInfo => abilitiesInfo.ability.name)
-            console.log(habilidade)
-            
                 accumulator += `
-                    <li class="card">
+                    <li class="card shadow-lg">
                         <img class="card-img ${types[0]} " alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
                         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                        <h4 class="card-subtitle">
-                            Tipo: ${types.join('|')}</h4>
-                        <h5 class="card-subtitle">Habilidades: ${habilidade.join(' | ')}</h5>
-                        <button class="btn btn-primary border-0 rounded-pill" type="button" id="captura" onclick="capturar('${pokemon.name}')">Capturar</button>
+                        <h5 class="card-subtitle ${types[0]}">
+                            Tipo: ${types.join('|')}
+                            <br/>
+                            Habilidades: ${habilidade.join(' | ')}
+                            </h5>
+                        <button class="btn btn-primary border-0 rounded-pill shadow-lg" type="button" id="captura" onclick="capturar('${pokemon.name}')">Capturar</button>
                     </li>
                 `
                 return accumulator
             }, '')
-
             const ul = document.querySelector('[data-js="pokedex"]')
-
             ul.innerHTML = lisPokemons
         })
-
 }
 
-
+//função de reload teste.
 function reload(){
     location.reload();
 }
 
+//Menuzinho de aviso de captura do pokemon.
 function capturar(nomePokemon){
     Swal.fire({
         position: 'center',
